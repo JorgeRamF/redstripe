2beb1fc0419a6d6a74fd7cc15f53cd5e3a9ca05a
JorgeRamF <padrequino@gmail.com>
2022-08-15 17:52:05 -0500

    fix
    fix

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -2,8 +2,7 @@ from odoo import models, fields, api
 
 class my_module(models.Model):
      _name = 'my_module.my_module'
-
-     name = fields.Char()
+      name = fields.Char()
      value = fields.Integer()
      value2 = fields.Float(compute="_value_pc", store=True)
      description = fields.Text()
